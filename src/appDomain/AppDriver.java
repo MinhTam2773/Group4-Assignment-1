67603be21f78d2d1c04edf78563c055c9a692aeb
Minh Tam <minhtam.nguyen@edu.sait.ca>
2025-10-01 21:23:31 -0600

    play around with eclipse terminal

--- src/appDomain/AppDriver.java
+++ src/appDomain/AppDriver.java
@@ -31,6 +31,8 @@ public class AppDriver
 		// refer to demo02 KittySort.java on how to use a custom sorting
 		// algorithm on a list of comparables to sort using either the
 		// natural order (comparable) or other orders (comparators)
+		
+		System.out.println("Hello world");
 
 	}
 
