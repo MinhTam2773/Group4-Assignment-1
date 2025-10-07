32c4226222392c25e42fdeedb0fc45194a945f2a
jhamrinal63@gmail.com <jhamrinal63@gmail.com>
2025-10-07 15:20:00 -0600

    done bubble sort

--- src/utilities/Sorter.java
+++ src/utilities/Sorter.java
@@ -1,7 +1,25 @@
 package utilities;
 
 public class Sorter {
-	public static void bubbleSort() {
+	public static void bubbleSort(Double[] array) {
+		int n = array.length;
+		boolean swap;
+		
+		for (int i = 0; i < n-1; i++) {
+			swap = false;
+			for (int j = 0; j< n-i-1; j++) {
+				if (array[j] < array[j+1]) {
+					double temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+					
+					swap = true;
+				}
+			}
+			if (!swap) {
+				break;
+			}
+		}
 		
 	}
 	
