ae59fa0d8517f2d578a7357c678bc20048ed79fe
AlexR4000 <Alexander.Raagas@edu.sait.ca>
2025-10-13 16:45:36 -0600

    Added methods for Octagonal, Pentagonal and Triangular prisms classes.

--- src/shapes/OctagonalPrism.java
+++ src/shapes/OctagonalPrism.java
@@ -1,6 +1,20 @@
 package shapes;
 
-public class OctagonalPrism
+public class OctagonalPrism extends Prism
 {
+	public OctagonalPrism(double height, double side) {
+		super(height, side);
+	}
 
+	@Override
+	public double calcVolume() {
+		// Volume = Base Area × Height
+		return calcBaseArea() * super.getHeight();
+	}
+
+	@Override
+	public double calcBaseArea() {
+		// Base Area (A) = 2(1 + √2)s²
+		return 2 * (1 + Math.sqrt(2)) * Math.pow(super.getSide(), 2);
+	}
 }
--- src/shapes/PentagonalPrism.java
+++ src/shapes/PentagonalPrism.java
@@ -1,6 +1,20 @@
 package shapes;
 
-public class PentagonalPrism
+public class PentagonalPrism extends Prism
 {
+	public PentagonalPrism(double height, double side) {
+		super(height, side);
+	}
 
+	@Override
+	public double calcVolume() {
+		// Volume = Base Area × Height
+		return calcBaseArea() * super.getHeight();
+	}
+
+	@Override
+	public double calcBaseArea() {
+		// Base Area (A) = [5s² × tan(54°)] / 4
+		return (5 * Math.pow(super.getSide(), 2) * Math.tan(Math.toRadians(54))) / 4;
+	}
 }
