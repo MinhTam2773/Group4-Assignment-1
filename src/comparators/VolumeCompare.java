a74af56f4d46a9f090cfc842a109bfc29bb6f02e
AlexR4000 <Alexander.Raagas@edu.sait.ca>
2025-10-13 17:19:17 -0600

    Added Volume Compare

--- src/comparators/VolumeCompare.java
+++ src/comparators/VolumeCompare.java
@@ -1,5 +1,22 @@
 package comparators;
 
-public class VolumeCompare {
-	
+import java.util.Comparator;
+import shapes.Shape;
+
+public class VolumeCompare implements Comparator<Shape> {
+
+    @Override
+    public int compare(Shape s1, Shape s2) {
+        double v1 = s1.calcVolume();
+        double v2 = s2.calcVolume();
+
+        // Return negative if s1 > s2 to sort descending
+        if (v1 > v2) {
+            return -1;
+        } else if (v1 < v2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
